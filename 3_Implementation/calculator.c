e50a171b6fc78c622a922b3f793bd6a881628dca
Lokesh Gupta <63411785+lokesh23799@users.noreply.github.com>
2022-02-11 10:19:16 +0530

    update code

--- 3_Implementation/calculator.c
+++ 3_Implementation/calculator.c
@@ -1,13 +1,13 @@
 #include<stdio.h> 
-int add(int x, int y) 
+int addition(int g, int h) 
 { 
 
  int c; 
- c=x+y; 
+ c=g+h; 
  printf("\nThe sum is=%d\n\n",c); 
 
 } 
-int sub(int d, int e) 
+int subtraction(int d, int e) 
 { 
 
  int f; 
@@ -15,60 +15,60 @@ int sub(int d, int e)
     printf("\nThe subtraction is=%d\n\n",f); 
 
 } 
-int mult(int x, int y) 
+int multiplication(int g, int h) 
 { 
 
  int i; 
- i=x*y; 
+ i=g*h; 
  printf("\nThe multiplication is=%d\n\n",i); 
 
 } 
-double div(double x, double y) 
+double division(double g, double h) 
 { 
 
     double m; 
-    m=x/y; 
+    m=g/h; 
     printf("\nDivison is=%.3lf\n\n",m); 
 
 } 
-int sq(int x, int y) 
+int square(int g, int h) 
 { 
 
     int sqr1,sqr2; 
-    sqr1=x*x; 
-    sqr2=y*y; 
-    printf("\nSquare of %d is=%d\n",x,sqr1); 
-    printf("\nSquare of %d is=%d\n\n",y,sqr2); 
+    sqr1=g*g; 
+    sqr2=h*h; 
+    printf("\nSquare of %d is=%d\n",g,sqr1); 
+    printf("\nSquare of %d is=%d\n\n",h,sqr2); 
 
 } 
-int cube(int x, int y) 
+int cube(int g, int h) 
 { 
 
     int cub1,cub2; 
-    cub1=x*x*x; 
-    cub2=y*y*y; 
-    printf("\nCube of %d is=%d\n",x,cub1); 
-    printf("\nCube of %d is=%d\n\n",y,cub2); 
+    cub1=g*g*g; 
+    cub2=h*h*h; 
+    printf("\nCube of %d is=%d\n",g,cub1); 
+    printf("\nCube of %d is=%d\n\n",h,cub2); 
 
 } 
-int percentage(int x,int y) 
+int percentage(int g,int h) 
 { 
 
     float per1,per2; 
-    per1=100*x/y; 
-    per2=100*y/x; 
-    printf("\n%d is %.3f %c of %d\n",x,per1,37,y);
-    printf("\n%d is %.3f %% of %d\n\n",y,per2,x);
+    per1=100*g/h; 
+    per2=100*h/g; 
+    printf("\n%d is %.3f %c of %d\n",g,per1,37,h);
+    printf("\n%d is %.3f %% of %d\n\n",h,per2,g);
 
 } 
  
 int main() 
 { 
 
- int x,y,options=0; 
+ int g,h,options=0; 
  printf("Enter numbers one by one:\n"); 
- scanf("%d%d",&x,&y); 
- printf("\nWelcome! What do you want to perform your calculator?\n"); 
+ scanf("%d%d",&g,&h); 
+ printf("\nWelcome! What do you want to perform in your calculator?\n"); 
  printf("\n1.For Addition enter 1\n\n2.For Subtraction enter 2\n"); 
  printf("\n3.For Multiplication enter 3\n\n4.For Division enter 4\n"); 
  printf("\n5.To find Square of the numbers enter 5\n\n6.To find Cube of the numbers enter 6\n"); 
@@ -78,25 +78,25 @@ int main()
  switch(options) 
  { 
  case 1: 
- add(x,y); 
+ addition(g,h); 
  break; 
  case 2: 
- sub(x,y); 
+ subtraction(g,h); 
  break; 
  case 3: 
- mult(x,y); 
+ multiplication(g,h); 
  break; 
  case 4: 
- div(x,y); 
+ division(g,h); 
  break; 
  case 5: 
- sq(x,y); 
+ square(g,h); 
  break; 
  case 6: 
- cube(x,y); 
+ cube(g,h); 
  break; 
  case 7: 
- percentage(x,y); 
+ percentage(g,h); 
  break; 
  default: 
     printf("\n\nPlease enter number from the given list!\n"); 
