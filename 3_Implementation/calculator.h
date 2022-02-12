1a4064a1273a3de462b45664f79735389c1c0a5f
Lokesh Gupta <63411785+lokesh23799@users.noreply.github.com>
2022-02-12 15:01:49 +0530

    commit

---
+++ 3_Implementation/calculator.h
@@ -0,0 +1,51 @@
+#include"calculator.h"
+
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+
+/**
+ * @brief addition
+ *
+ * 
+ */
+
+int addition(int g, int h)
+
+/**
+ * @brief subtraction
+ * 
+ */
+int subtraction(int d, int e) 
+
+/**
+ * @brief multiplication
+ * 
+ */
+int multiplication(int g, int h)
+
+/**
+ * @brief division
+ * 
+ */
+double division(double g, double h)
+
+/**
+ * @brief square
+ * 
+ */
+int square(int g, int h) 
+
+/**
+ * @brief cube
+ * 
+ */
+int cube(int g, int h)
+
+/**
+ * @brief percentage
+ * 
+ */
+int percentage(int g,int h)
+
+# endif
---
+++ 3_Implementation/calculator_main.c
@@ -0,0 +1,45 @@
+#include"calculator.h"
+
+int main() 
+{ 
+
+ int g,h,options=0; 
+ printf("Enter numbers one by one:\n"); 
+ scanf("%d%d",&g,&h); 
+ printf("\nWelcome! What do you want to perform in your calculator?\n"); 
+ printf("\n1.For Addition enter 1\n\n2.For Subtraction enter 2\n"); 
+ printf("\n3.For Multiplication enter 3\n\n4.For Division enter 4\n"); 
+ printf("\n5.To find Square of the numbers enter 5\n\n6.To find Cube of the numbers enter 6\n"); 
+ printf("\n7.For Percentage of both numbers enter 7\n"); 
+ printf("\nEnter your choice:"); 
+ scanf("%d",&options); 
+ switch(options) 
+ { 
+ case 1: 
+ addition(g,h); 
+ break; 
+ case 2: 
+ subtraction(g,h); 
+ break; 
+ case 3: 
+ multiplication(g,h); 
+ break; 
+ case 4: 
+ division(g,h); 
+ break; 
+ case 5: 
+ square(g,h); 
+ break; 
+ case 6: 
+ cube(g,h); 
+ break; 
+ case 7: 
+ percentage(g,h); 
+ break; 
+ default: 
+    printf("\n\nPlease enter number from the given list!\n"); 
+    
+    
+} 
+
+} 
